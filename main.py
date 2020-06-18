ac34a55db7da2db20c00be07d581a729d35dcfd9
Kevin Ernest Long <kevinelong@gmail.com>
2020-06-18 11:02:28 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+"""
+TDD VIDEO GAME REQUIREMENTS
+Hero ship at bottom can move left right and fire.
+Enemy a block of rows and columns of enemy ships move left or right together,
+ and then descend and reverse when the group gets near a wall.
+"""
